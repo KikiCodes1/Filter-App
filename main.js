9f28d3a35407ec2f00a2da924974c4747e1619bc
KikiCodes1 <95891807+KikiCodes1@users.noreply.github.com>
2022-12-19 18:44:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
-function preload(){
+noseX = 0;
+noseY = 0;
 
+function preload(){
+    glasses_mustache = loadImage("https://i.postimg.cc/wxGyDT6c/glasses-nose-and-mustache-removebg-preview.png");
 }
 
 function setup(){
-    canvas = createCanvas(640, 480);
-    canvas.position(110, 250);
+    canvas = createCanvas(400, 400);
+    canvas.center();
     video = createCapture(VIDEO);
+    video.size(400, 400);
     video.hide();
 
-    tint_color = "";
+    poseNet = ml5.poseNet(video, modeLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw(){
-    image(video, 0, 0, 640, 480);
-    tint(tint_color);
+    image(video, 0, 0, 400, 400);
+    image(glasses_mustache, noseX - 55, noseY - 70, 120, 120);
 }
 
-function take_snapshot(){
-    save('filter_photo.png');
+function take_picture(){
+    save('filterImage.png');
 }
 
-function filter_tint(){
-    tint_color = document.getElementById("color_name").value;
+function modeLoaded(){
+    console.log("PoseNet has been initialized!");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
 }
